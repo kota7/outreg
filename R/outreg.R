a743c2a52b800b1c9fd0793a26d932b9ec991237
kota <kmori05@gmail.com>
2017-03-11 22:04:46 +0900

    more examples, stats choice option

--- R/outreg.R
+++ R/outreg.R
@@ -4,20 +4,34 @@
 #' @param fitlist list of regression outcomes
 #' @param digits number of digit to real numbers
 #' @param alpha  vector of significance levels to star
-#' @param displayed vector of stats names to display
+#' @param coef show coefficients
+#' @param se show standard errors
+#' @param pv show p values
+#' @param tv show t values
 #' @param bracket stats to be in brackets
-#' @param starred stats to star
+#' @param starred stats to put stars on
 #' @param robust if TRUE, robust standard error is used
 #' @param small if TRUE, small sample parameter distribution is used
+#' @return regression table in data.frame format
 #' @examples
 #' fitlist <- list(lm(mpg ~ cyl, data = mtcars),
 #'                 lm(mpg ~ cyl + wt + hp, data = mtcars),
 #'                 lm(mpg ~ cyl + wt + hp + drat, data = mtcars))
 #' outreg(fitlist)
+#'
+#' # with custom regression names
+#' outreg(setNames(fitlist, c('small', 'medium', 'large')))
+#'
+#' # star on standard errors, instead of estimate
+#' outreg(fitlist, starred = 'se')
+#'
+#' # include other stats
+#' outreg(fitlist, pv = TRUE, tv = TRUE, se = FALSE)
+#'
 #' @export
 outreg <- function(fitlist,
                    digits = 3L, alpha = c(0.1, 0.05, 0.01),
-                   displayed = c('coef', 'se', 'nobs', 'R2', 'adjR2'),
+                   coef = TRUE, se = TRUE, pv = FALSE, tv = FALSE,
                    bracket = c('se'), starred = c('coef'),
                    robust = FALSE, small = TRUE)
 {
@@ -52,12 +66,18 @@ outreg <- function(fitlist,
     tidyr::spread_(key = 'modelname', value = 'value', fill = '')
   stat_df_reshaped$statname <- as.character(stat_df_reshaped$statname)
 
-  # add dummy variable column to stat
+  # add dummy variable column to stat part
+  # so as to consistently bind with coef part
   stat_df_reshaped$variable = ''
   stat_df_reshaped <- stat_df_reshaped[names(coef_df_reshaped)]
 
   out <- rbind(coef_df_reshaped, stat_df_reshaped)
 
+  displayed <- unique(out$statname)
+  if (!coef) displayed <- setdiff(displayed, 'coef')
+  if (!se)   displayed <- setdiff(displayed, 'se')
+  if (!pv)   displayed <- setdiff(displayed, 'pv')
+  if (!tv)   displayed <- setdiff(displayed, 'tv')
   out <- out[out$statname %in% displayed,]
   out$statname <- .display_names[out$statname] %>% unlist() %>% unname()
 
